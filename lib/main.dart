878111150f27c674ab863bd51df3d22791453fe5
Toushik Banik <toushikbanikeducation@gmail.com>
2022-02-26 13:02:50 +0530

    On SetState

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ void main() {
     return runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
           appBar: AppBar(
             title: const Center(child: Text('Dicee'),),
-            backgroundColor: Colors.red,
+            backgroundColor: Colors.blue,
           ),
           body: DicePage(),
         ),
@@ -17,26 +17,40 @@ void main() {
   }
 }
 
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
-class DicePage extends StatelessWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int randomNumber=1;
   @override
   Widget build(BuildContext context) {
-    var randomNumber = 2;
     return SafeArea(child: Center(
       child: Row(
         children: [
           Expanded(
             flex: 1,
             child: TextButton(
-            onPressed: () { print('hi'); },
-            child: Image.asset('images/dice$randomNumber.png'),
-          ),
+              onPressed: () {
+                setState(() {
+                  randomNumber = 6;
+                });
+                },
+              child: Image.asset('images/dice$randomNumber.png'),
+            ),
           ),
           Expanded(
             flex: 1,
             child:
             TextButton(
-              onPressed: () { print('hi'); },
+              onPressed: (){
+                setState(() {
+                  randomNumber = 2;
+                });
+              },
               child: Image.asset('images/dice$randomNumber.png'),
             ),
           ),
@@ -45,4 +59,5 @@ class DicePage extends StatelessWidget {
     ),
     );
   }
+
 }
